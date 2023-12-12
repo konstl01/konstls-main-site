f0c69e6b52d65e50dea3d04494521630748fa35c
konstl01 <konstantinrak01@gmail.com>
2023-12-12 17:11:19 +0100

    add clock and title changer

--- js/app.js
+++ js/app.js
@@ -96,3 +96,30 @@ if(window.innerWidth >= 725) {
 } else {
     main.style.maxHeight = `${window.innerWidth * 1.6}px`;
 }
+
+// Automatic Title change
+
+window.addEventListener("blur", () => {
+    document.title = "Come back!";
+})
+window.addEventListener("focus", () => {
+    document.title = "Pine Trees";
+})
+
+// Clock
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
